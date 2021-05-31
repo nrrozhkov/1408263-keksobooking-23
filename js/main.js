d72fa5d42c1b69b2e5c1d3fd7a34fcf87c275493
nrrozhkov <nrrozhkov@gmail.com>
2021-05-31 17:52:04 +0300

    Внес правки в формулы

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-function rangeCheck(min, max) {
+// Функция возвращает случайное число
+function getRandomInt(min, max) {
+  if (max === undefined) {
+    return getRandomInt(0, min);
+  }
   if (min >= 0 && max >= 0) {
     const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
-  } else {
-    return(NaN);
   }
 }
 
-rangeCheck();
+getRandomInt();
 
-
-function getRandomCoordinate(min, max) {
-  return Math.random() * (max - min) + min;
+// Функция возвращает случайное число с плавающей точкой
+function getRandomDecimal(min, max, decimalDigits) {
+  if (max === undefined) {
+    return getRandomDecimal(0, min);
+  }
+  if (min >= 0 && max >= 0) {
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
+    return rand.toFixed(decimalDigits);
+  }
 }
+getRandomDecimal(2, 3, 100);
 
-getRandomCoordinate();
