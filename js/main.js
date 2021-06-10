5aa5b4b70f894d15dac692ff81192142a4b2c9e3
nrrozhkov <nrrozhkov@gmail.com>
2021-06-10 12:44:57 +0300

    Внес правки в генерацию дефолтных данных

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ getRandomPositiveInteger();
 
 //Функция, возвращающая случайное целое число c плавающей запятой
 // eslint-disable-next-line id-length
-function getRandomPositiveFloat(a, b, digits = 1) {
+function getRandomPositiveFloat(a, b, digits = 6) {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
   const result = Math.random() * (upper - lower) + lower;
@@ -19,81 +19,62 @@ function getRandomPositiveFloat(a, b, digits = 1) {
 }
 
 getRandomPositiveFloat();
-
-const numbers = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const maxPrice = 1000000;
+const randomItem = 10;
+const numbers = [...Array(10)].map((item, ij) => (`${ij + 1  }`).padStart(2, 0));
 //Функция для генерации линка пользователя
-const avatar = function () {
+const getAvatar = function () {
   const link = 'img/avatars/user';
   const format = '.png';
   return link + numbers.pop() + format;
 };
 
-const lat = function () {
-  return getRandomPositiveFloat(35.65000, 35.70000);
-};
+const getLat = () => getRandomPositiveFloat(35.65, 35.7);
 
-const lng = function () {
-  return getRandomPositiveFloat(139.70000, 139.80000);
-};
+const getLng = () => getRandomPositiveFloat(139.7, 139.8);
 
+const getTitle = () => 'Дом, который построил Джек';
 
-const title = () => 'Дом, который построил Джек';
-
-const address = function () {
-  return `${lat()} ${lng()}`;
+const getAddress = function () {
+  return `${getLat()} ${getLng()}`;
 };
 const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkin = ['12:00', '13:00', '14:00'];
-const checkout = ['12:00', '13:00', '14:00'];
-const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const shuffle = function (array) {
-  let currentIndex = array.length, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-};
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-
-const author = function () {
-  return avatar();
+const checkinIntervals = [12, 13, 14];
+const checkoutIntervals = [12, 13, 14];
+const featuresLists = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const featuresListsShuffles = [...featuresLists].sort(() => Math.random() - 0.5);
+const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const photoShuffles = [...photos].sort(() => Math.random() - 0.5);
+const getAuthor = function () {
+  return getAvatar();
 };
 
-const offer = function () {
+const getOffer = function () {
   return {
-    title: title(),
-    address: address(),
-    price: getRandomPositiveInteger(0, 1000000),
+    title: getTitle(),
+    address: getAddress(),
+    price: getRandomPositiveInteger(0, maxPrice),
     type: type[getRandomPositiveInteger(0, type.length - 1)],
     rooms: getRandomPositiveInteger(0, 10),
     guests: getRandomPositiveInteger(0, 100),
-    checkin: checkin[getRandomPositiveInteger(0, checkin.length - 1)],
-    checkout: checkout[getRandomPositiveInteger(0, checkout.length - 1)],
-    features: shuffle(featuresList),
+    checkin: checkinIntervals[getRandomPositiveInteger(0, checkinIntervals.length - 1)],
+    checkout: checkoutIntervals[getRandomPositiveInteger(0, checkoutIntervals.length - 1)],
+    features: featuresListsShuffles,
     description: 'Здесь будет описание',
-    photos: shuffle(photos),
+    photos: photoShuffles,
   };
 };
 
-const locationValue = function () {
-  return `${lat()} ${lng()}`;
+const getLocationValue = function () {
+  return `${getLat()} ${getLng()}`;
 };
 
 // eslint-disable-next-line no-unused-vars
-const randomData = new Array(10).fill(null).map(() => ({
-  author: author(),
-  offer: offer(),
-  location: locationValue(),
+const randomItems = [...Array(randomItem)].fill(null).map(() => ({
+  author: getAuthor(),
+  offer: getOffer(),
+  location: getLocationValue(),
 }));
-
-
+console.log(randomItems);
