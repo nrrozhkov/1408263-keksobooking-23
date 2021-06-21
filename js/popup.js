092d2cce837869a0d5605a58c0a6f52237245bf5
nrrozhkov <nrrozhkov@gmail.com>
2021-06-21 22:15:03 +0300

    Внес правки

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,26 @@
 import { randomItems } from './data.js';
+// import { photos } from './util.js';
 const container = document.querySelector('#map-canvas');
 const card = document.querySelector('#card').content.querySelector('.popup');
 const HOUSING_TYPE_DICTIONARY = { bungalow: 'Бунгало', flat: 'Квартира', hotel: 'Отель',  house: 'Дом', palace: 'Дворец' };
 const houseType = (name) => HOUSING_TYPE_DICTIONARY[name];
+
 const createPopupImage = (imageArray, imageContainer) => {
   if (imageArray) {
     imageArray.forEach((photoSrc) => {
       const photo = document.createElement('img');
-
+      const width = 45;
+      const height = 40;
       photo.classList.add('popup__photo');
-      photo.width = 45;
-      photo.height = 40;
+      photo.width = width;
+      photo.height = height;
       photo.alt = 'Фотография жилья';
       photo.src = photoSrc;
       imageContainer.appendChild(photo);
-      console.log(photoSrc);
-      console.log(photo.src);
     });
   }
 };
-const contentCheck = (content) => {
+const contentEmptyCheck = (content) => {
   if (!content.textContent.length) {
     content.remove();
   }
@@ -28,7 +29,8 @@ const newArticle = randomItems();
 
 newArticle.forEach((element) => {
   const article = card.cloneNode(true);
-  const photoContainer = article.querySelector('.popup__photo');
+  const photoContainer = article.querySelector('.popup__photos');
+  contentEmptyCheck(article);
 
   article.querySelector('.popup__title').textContent = element.offer.title;
   article.querySelector('.popup__text--address').textContent = element.offer.address;
@@ -38,11 +40,8 @@ newArticle.forEach((element) => {
   article.querySelector('.popup__text--time').textContent = `Заезд после ${  element.offer.checkin  } выезд до ${  element.offer.checkout}`;
   article.querySelector('.popup__features').textContent = element.offer.features;
   article.querySelector('.popup__description').textContent = element.offer.description;
-  article.querySelector('.popup__photo').textContent = createPopupImage(element.offer.photoShuffles, photoContainer);
-  console.log(createPopupImage(element.offer.photoShuffles, photoContainer));
+  createPopupImage(element.offer.photos, photoContainer);
   article.querySelector('.popup__description').src = element.author;
-
-  contentCheck(article);
   container.appendChild(article);
 });
 
