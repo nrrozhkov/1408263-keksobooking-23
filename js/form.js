f43c4a0d17c0ca5c148d47fc7a59f540fd9dd571
nrrozhkov <nrrozhkov@gmail.com>
2021-07-15 11:16:00 +0300

    Update form.js
    Поправил согласно комментариям

--- js/form.js
+++ js/form.js
@@ -18,97 +18,73 @@ const mapLoadCheck = () => {
   });
 };
 
-overnightPrice.addEventListener('input', () => {
-  const valuePrice = overnightPrice.value;
-  if (houseTypeInput.value === 'bungalow') {
-    if (valuePrice < '0') {
+const valueValidity = () => {
+  debugger;
+  const value = houseTypeInput.value;
+  const valuePrice = Number(overnightPrice.value);
+  if (value === 'bungalow') {
+    if (valuePrice < 0) {
       overnightPrice.setCustomValidity('Не меньше 1 руб.');
       return false;
     }
   }
-  if (houseTypeInput.value === 'flat') {
-    if (valuePrice < '1000') {
+  if (value === 'flat') {
+    if (valuePrice < 1000) {
       overnightPrice.setCustomValidity('Не меньше 1000 руб.');
       return false;
     }
   }
-  if (houseTypeInput.value === 'hotel') {
-    if (valuePrice < '3000') {
+  if (value === 'hotel') {
+    if (valuePrice < 3000) {
       overnightPrice.setCustomValidity('Не меньше 3000 руб.');
       return false;
     }
   }
-  if (houseTypeInput.value === 'house') {
-    if (valuePrice < '5000') {
+  if (value === 'house') {
+    if (valuePrice < 5000) {
       overnightPrice.setCustomValidity('Не меньше 5000 руб.');
       return false;
     }
   }
-  if (houseTypeInput.value === 'palace') {
-    if (valuePrice < '10000') {
+  if (value === 'palace') {
+    if (valuePrice < 10000) {
       overnightPrice.setCustomValidity('Не меньше 10000 руб.');
       return false;
     }
   }
+};
+overnightPrice.addEventListener('input', () => {
+  valueValidity();
   overnightPrice.setCustomValidity('');
 });
 
 houseTypeInput.addEventListener('input', () => {
-  const valuePrice = overnightPrice.value;
-  if (houseTypeInput.value === 'bungalow') {
-    if (valuePrice < '0') {
-      overnightPrice.setCustomValidity('Не меньше 1 руб.');
-      return false;
-    }
-  }
-  if (houseTypeInput.value === 'flat') {
-    if (valuePrice < '1000') {
-      overnightPrice.setCustomValidity('Не меньше 1000 руб.');
-      return false;
-    }
-  }
-  if (houseTypeInput.value === 'hotel') {
-    if (valuePrice < '3000') {
-      overnightPrice.setCustomValidity('Не меньше 3000 руб.');
-      return false;
-    }
-  }
-  if (houseTypeInput.value === 'house') {
-    if (valuePrice < '5000') {
-      overnightPrice.setCustomValidity('Не меньше 5000 руб.');
-      return false;
-    }
-  }
-  if (houseTypeInput.value === 'palace') {
-    if (valuePrice < '10000') {
-      overnightPrice.setCustomValidity('Не меньше 10000 руб.');
-      return false;
-    }
-  }
+  valueValidity();
   houseTypeInput.setCustomValidity('');
 });
 
+const setDisabled = (value) => {
+  const houseCapacityValue = houseCapacity.options[value];
+  return houseCapacityValue.disabled = true;
+};
+setDisabled(0);
+const setEnabled = (value) => {
+  const houseCapacityValue = houseCapacity.options[value];
+  return houseCapacityValue.disabled = false;
+};
 roomsCounter.addEventListener('change', () => {
-  const roomValue = roomsCounter.value;
-  const houseCapacityValue3 = () => (houseCapacity.options[0].disabled = true);
-  const houseCapacityValue2 = () => (houseCapacity.options[1].disabled = true);
-  const houseCapacityValue1 = () => (houseCapacity.options[2].disabled = true);
-  const houseCapacityValue0 = () => (houseCapacity.options[3].disabled = true);
-  const houseCapacityValue3True = () => (houseCapacity.options[0].disabled = false);
-  const houseCapacityValue2True = () => (houseCapacity.options[1].disabled = false);
-  const houseCapacityValue1True = () => (houseCapacity.options[2].disabled = false);
-  const houseCapacityValue0True = () => (houseCapacity.options[3].disabled = false);
-  if (roomValue === '1') {
-    houseCapacityValue0(), houseCapacityValue2(), houseCapacityValue3(), houseCapacityValue1True();
+  const roomValue = Number(roomsCounter.value);
+  if (roomValue === 1) {
+    setDisabled(3), setDisabled(1), setDisabled(0), setEnabled(2);
   }
-  if (roomValue === '2') {
-    houseCapacityValue0(), houseCapacityValue3(), houseCapacityValue1True(), houseCapacityValue2True();
+  if (roomValue === 2) {
+    setDisabled(3), setDisabled(0), setEnabled(3), setEnabled(1);
   }
-  if (roomValue === '3') {
-    houseCapacityValue0(), houseCapacityValue3True(), houseCapacityValue1True(), houseCapacityValue2True();
+  if (roomValue === 3) {
+    setDisabled(3), setEnabled(0), setEnabled(2), setEnabled(1);
   }
-  if (roomValue === '100') {
-    houseCapacityValue1(), houseCapacityValue2(), houseCapacityValue3(), houseCapacityValue0True();
+  if (roomValue === 100) {
+    setDisabled(2), setDisabled(1), setDisabled(0), setEnabled(3);
   }
 });
 
